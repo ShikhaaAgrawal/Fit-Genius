054d7ebb2a6722a70df6f5b7bfce202a7c8d35c9
Muskan Gupta <116276756+muskangupta123@users.noreply.github.com>
2025-02-07 21:08:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import MainContent from "./components/mainc";
+//import NutriVision from "./pages/nutriVision";
+import AlignFit from "./pages/alignFit";
+// import HarmonyHub from "./pages/harmonyHub";
+//import FoodUpload from "./pages/FoodUpload";
+//import RealTimeDetection from './pages/RealTimeDetection';
+//import RecipeGenerator from "./pages/RecipeGenerator";
 import Footer from "./components/footer";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <br></br>
-      <br></br>
-      <MainContent />
+    <Router>
+      <Navbar /> {/* Navbar should be inside Router */}
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+        //<Route path="/nutrivision" element={<NutriVision />} />
+        <Route path="/alignfit" element={<AlignFit />} />
+        //<Route path="/recipe-generator" element={<RecipeGenerator />} />
+        {/* <Route path="/harmonyhub" element={<HarmonyHub />} /> */}
+        //<Route path="/upload-food" element={<FoodUpload />} /> {/* Fixed route */}
+        //<Route path="/real-time-detection" element={<RealTimeDetection />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
